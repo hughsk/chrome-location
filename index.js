d6b3f78a44f771472c71c86b84e204c324e4fb38
Hugh Kennedy <hughskennedy@gmail.com>
2014-10-09 00:35:50 +0200

    .

---
+++ index.js
@@ -0,0 +1,23 @@
+module.exports = null
+
+if (process.platform === 'darwin')
+  return module.exports = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+
+if (process.platform !== 'win32')
+  return module.exports = 'google-chrome'
+
+var fs = require('fs')
+
+var suffix = '\\Google\\Chrome\\Application\\chrome.exe';
+var prefixes = [
+    process.env.LOCALAPPDATA
+  , process.env.PROGRAMFILES
+  , process.env['PROGRAMFILES(X86)']
+]
+
+for (var i = 0; i < prefixes.length; i++) {
+  var exe = prefixes[i] + suffix
+  if (fs.existsSync(exe)) {
+    return module.exports = exe
+  }
+}
