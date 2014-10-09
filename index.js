2b6dcecc2a27af4eb35ee8dc93e2dd980f4eb73b
Hugh Kennedy <hughskennedy@gmail.com>
2014-10-09 17:31:40 +0200

    use userhome, shuffle things around

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
-var fs = require('fs')
+var osx   = process.platform === 'darwin'
+var win   = process.platform === 'win32'
+var other = !osx && !win
+var fs    = require('fs')
 
-module.exports = null
+if (other) {
+  module.exports = require('which').sync('google-chrome')
+} else
+if (osx) {
+  var regPath = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+  var altPath = require('userhome')(regPath.slice(1))
 
-if (process.platform === 'darwin') {
-  var regPath = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome';
-  var altPath = '~' + regPath;
-
-  return module.exports = fs.existsSync(regPath)
+  module.exports = fs.existsSync(regPath)
     ? regPath
     : altPath
-}
+} else {
+  var suffix = '\\Google\\Chrome\\Application\\chrome.exe';
+  var prefixes = [
+      process.env.LOCALAPPDATA
+    , process.env.PROGRAMFILES
+    , process.env['PROGRAMFILES(X86)']
+  ]
 
-
-if (process.platform !== 'win32')
-  return module.exports = require('which').sync('google-chrome')
-
-
-var suffix = '\\Google\\Chrome\\Application\\chrome.exe';
-var prefixes = [
-    process.env.LOCALAPPDATA
-  , process.env.PROGRAMFILES
-  , process.env['PROGRAMFILES(X86)']
-]
-
-for (var i = 0; i < prefixes.length; i++) {
-  var exe = prefixes[i] + suffix
-  if (fs.existsSync(exe)) {
-    return module.exports = exe
+  for (var i = 0; i < prefixes.length; i++) {
+    var exe = prefixes[i] + suffix
+    if (fs.existsSync(exe)) {
+      module.exports = exe
+      break
+    }
   }
 }
